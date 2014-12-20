af0c2f0c65fa2492bd654a5002b2ee8d9caa8cc0
Charlike Mike Reagent <mamemto_100@mail.bg>
2014-12-20 16:39:22 +0200

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * init-package <https://github.com/tunnckoCore/init-package>
+ *
+ * Copyright (c) 2014 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict';
+
+var path = require('path');
+var co = require('co');
+var prompt = require('co-prompt');
+var meow = require('meow');
+
+module.exports = function initPackage() {
+  // body
+};
---
+++ test.js
@@ -0,0 +1,14 @@
+/**
+ * init-package <https://github.com/tunnckoCore/init-package>
+ *
+ * Copyright (c) 2014 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict';
+
+var initPackage = require('./index');
+
+describe('init-package:', function() {
+  // body
+});
